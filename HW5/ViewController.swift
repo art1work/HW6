78bc1e8db85d4ae6dda617f0ba450c0729db89e2
art1work <avermokhin@gmail.com>
2022-07-03 12:12:06 +0300

    Initial Commit

---
+++ HW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5
+//
+//  Created by Артём Ермохин on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
